078895363362563a338fd2a60e73fd578c3ad805
pckalpha <github@phrodide.com>
2023-03-05 21:53:13 -0700

    Before Image Processing Overhaul

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,50 @@
 const htmlmin = require('html-minifier');
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
+const Image = require("@11ty/eleventy-img");
+const path = require('path');
 
 function eleventyConfig(config) {
 	// Passthroughs
 	config.addPassthroughCopy("src/img");
+	config.addPassthroughCopy("src/Blog/.attachments");
 
 	// Layout aliases
 	config.addLayoutAlias("base", "layouts/base.njk");
+
+
+// --- START, eleventy-img
+function imageShortcode(src, alt, sizes="(min-width: 1024px) 100vw, 50vw") {
+	console.log(`Generating image(s) from:  ${src}`)
+	let options = {
+		widths: [600, 900, 1500],
+		formats: ["webp", "jpeg"],
+		//urlPath: "/Blog/",
+		outputDir: "./dist/img/",
+		filenameFormat: function (id, src, width, format, options) {
+			const extension = path.extname(src)
+			const name = path.basename(src, extension)
+			return `${name}-${width}w.${format}`
+		}
+	}
+
+	// generate images
+	Image(src, options)
+
+	let imageAttributes = {
+		alt,
+		sizes,
+		loading: "lazy",
+		decoding: "async",
+	}
+	// get metadata
+	metadata = Image.statsSync(src, options)
+	return Image.generateHTML(metadata, imageAttributes)
+}
+// --- END, eleventy-img
+
+
+
 
 	// Minify HTML
 	const isProduction = process.env.ELEVENTY_ENV === "production";
@@ -26,6 +63,7 @@ function eleventyConfig(config) {
 		html: true
 	});
 	
+	//config.addShortcode("image", imageShortcode)
 	config.addPairedShortcode("markdown", (content) => {
 		return md.render(content);
 	});
